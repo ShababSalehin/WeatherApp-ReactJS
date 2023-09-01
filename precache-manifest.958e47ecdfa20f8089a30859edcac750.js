fc87df422e6c8c7b69176f44d2de33ab61ffe5b1
Shabab Salehin <salehin4axiz@gmail.com>
2023-09-01 18:24:33 +0600

    Updates

---
+++ precache-manifest.958e47ecdfa20f8089a30859edcac750.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe048b119b5a159eaae9e7fe25d5bd2b",
+    "url": "/WeatherApp-ReactJS/index.html"
+  },
+  {
+    "revision": "0b63c18e3c1770028e10",
+    "url": "/WeatherApp-ReactJS/static/css/main.047c0e8d.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/WeatherApp-ReactJS/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp-ReactJS/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b63c18e3c1770028e10",
+    "url": "/WeatherApp-ReactJS/static/js/main.9030bb9f.chunk.js"
+  },
+  {
+    "revision": "4a9231035018b3dd7370",
+    "url": "/WeatherApp-ReactJS/static/js/runtime-main.0456ae07.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/WeatherApp-ReactJS/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/WeatherApp-ReactJS/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/WeatherApp-ReactJS/static/media/city.e1698332.jpg"
+  }
+]);
